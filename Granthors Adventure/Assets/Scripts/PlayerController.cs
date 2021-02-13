a3f2f32a48e95e96d5463e2681174bdf3c2d332f
Jason Keller <kellej6@ucmail.uc.edu>
2021-02-13 14:57:40 -0500

    Added animations
    added player animations and corrected several collision bugs.

--- Granthors Adventure/Assets/Scripts/PlayerController.cs
+++ Granthors Adventure/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 5.0f;
+    public Rigidbody2D rigidbody;
+    public Animator animator;
+
+    Vector2 movement;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +19,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
 
-        Vector2 position = transform.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-        transform.position = position;
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    // FixedUpdate is called once per frame at a fixed rate (50 frames/sec)
+    private void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
